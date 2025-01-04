1faa76017c5a50bbd027e93ba7572b9804f79774
rosafy <emmadomeisen@gmail.com>
2025-01-04 23:04:44 +0100

    Started work on GUI

--- gui.py
+++ gui.py
@@ -1,8 +1,16 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter import filedialog as fd
+def compile():
+    codefile = fd.askopenfilename()
 root = Tk()
-frm = ttk.Frame(root, padding=10)
-frm.grid()
-ttk.Label(frm, text="nmcfs compiler").grid(column=0, row=0)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+root.title("nmcfs Compiler")
+root.resizable(width=False, height=False)
+frame1 = Frame(root, width=300, height=300)
+frame1.grid(row=0, column=0)
+label1 = Label(frame1, text="Compile nmcfs code",
+                  fg="white", font=("Arial", 12, "bold"))
+label1.place(relx=0.5, rely=0.1, anchor=CENTER)
+ttk.Button(frame1, text="Quit", command=root.destroy).grid(column=1, row=0)
+ttk.Button(frame1, text="Compile", command=compile).grid(column=2, row=0)
 root.mainloop()
