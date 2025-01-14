a29ebed506a2ca1eddefe1f44c25d5cdb92e7fbb
rosafy <emmadomeisen@gmail.com>
2025-01-14 17:12:08 +0100

    Worked und cmdui

--- cmdui.py
+++ cmdui.py
@@ -1,2 +1,7 @@
 from parser import *
+import time
+print("nmcfs Compiler")
+time.sleep(0.5)
+print("© Rosafy Non-Commercial Permissive License (RNCPL) - 2025 Rosafy")
+time.sleep(0.5)
 filepath = input("Paste path of file to compile: ")
