2b680b25796ff29140344a3c30d4678bc3689872
rosafy <emmadomeisen@gmail.com>
2025-01-19 20:21:27 +0100

    Fixed Scaling (I hope)

--- gui.py
+++ gui.py
@@ -1,4 +1,3 @@
-# Import Modules
 from tkinter import *
 from tkinter import ttk
 from tkinter import filedialog as fd
@@ -6,78 +5,112 @@ from PIL import Image, ImageTk
 import sv_ttk
 import webbrowser
 from tktooltip import ToolTip
-import parser
-
-
 
 # Functions
-def selectfile(): # Function called by "Select" Button
+def selectfile():
     global codefile
     codefile = fd.askopenfilename()
-    pathinput.delete(0,END)
-    pathinput.insert(0,codefile)
+    pathinput.delete(0, END)
+    pathinput.insert(0, codefile)
 
-def selectdir(): # Function called by second "Select" Button
+def selectdir():
     global outdir
     outdir = fd.askdirectory()
-    outdirinput.delete(0,END)
-    outdirinput.insert(0,outdir)
+    outdirinput.delete(0, END)
+    outdirinput.insert(0, outdir)
 
-def compile(): # Function called by "Compile" Button
+def compile():
     print("test")
 
-def urlopen(url): # Function used for License Link
+def urlopen(url):
     webbrowser.open_new_tab(url)
 
 # Window Setup
 root = Tk()
 root.title("nmcfs Compiler")
-root.resizable(width=False, height=False)
+root.geometry("300x500")
+root.resizable(True, True)
+root.minsize(300,500)
 sv_ttk.set_theme("light")
-frame1 = ttk.Frame(root, width=300, height=500)
 im = Image.open('nmcfs_templogo.png')
 photo = ImageTk.PhotoImage(im)
-root.wm_iconphoto(True, photo) 
-frame1.grid(row=0, column=0)
-frame1.grid_propagate(0)
-frame1.update()
+root.wm_iconphoto(True, photo)
 
-# Logo
+# DPI Scaling
+scaling_factor = root.tk.call("tk", "scaling")
+def scale(value):
+    if type(value) is int or type(value) is float or type(value) is complex:
+        return max(int(value * scaling_factor), 1)
+    elif type(value) is tuple:
+        returnvalue = (max(value[0]*scaling_factor,1),max(value[1]*scaling_factor,1))
+        return returnvalue
+    else:
+        raise Exception("Invalid Data Type")
+    
+# Frame Setup
+frame1 = ttk.Frame(root)
+frame1.grid(row=0, column=0, sticky="nsew", padx=scale(10), pady=scale(10))
+
+# Configure Grid
+root.grid_rowconfigure(0, weight=1)
+root.grid_columnconfigure(0, weight=1)
+frame1.grid_rowconfigure(8, weight=1)
+frame1.grid_columnconfigure(0, weight=1)
+frame1.grid_columnconfigure(1, weight=1)
+
+# Logo Setup
 logoimg = Image.open("nmcfs_templogo.png")
-logoimg = logoimg.resize((100,100))
+logoimg = logoimg.resize((scale(50), scale(50)), Image.Resampling.LANCZOS)
 logoimg = ImageTk.PhotoImage(logoimg)
 logo = ttk.Label(frame1, image=logoimg)
-logo.place(x=frame1.winfo_width()/2-50,y=5)
+logo.grid(row=0, column=0, columnspan=3)
 
 # Main UI Elements
-l = ttk.Label(frame1,text="nmcfs Compiler",font=("Helvetica",15,"bold"))
-l.place(x=frame1.winfo_width()/2, y=120, anchor="center")
-quitbutton = ttk.Button(frame1, text="Quit", command=root.destroy, width=15)
-quitbutton.place(x=160,y=420)
-compilebutton = ttk.Button(frame1, text="Compile", command=compile, width=15)
-compilebutton.place(x=10,y=420)
+l = ttk.Label(frame1, text="nmcfs Compiler", font=("Helvetica", scale(8), "bold"))
+l.grid(row=1, column=0, columnspan=3)
 
 # Input Path
-pathlabel1 = ttk.Label(frame1,text="File to compile:",font=("Helvetica",7,"bold"))
-pathlabel1.place(x=10,y=200)
-pathinput = ttk.Entry(frame1, width=19)
-pathinput.place(x=10,y=220,height=30)
-pathbutton = ttk.Button(frame1,text="Select",command=selectfile)
-pathbutton.place(x=230,y=220)
+pathlabel1 = ttk.Label(frame1, text="File to compile:", font=("Helvetica", scale(4), "bold"))
+pathlabel1.grid(row=2, column=0, sticky="w",pady=scale((30,0)),columnspan=2)
+
+pathinput = ttk.Entry(frame1)
+pathinput.grid(row=3, column=0, sticky="ew", columnspan=2,ipadx=scale(2000))
+
+pathbutton = ttk.Button(frame1, text="Select", command=selectfile)
+pathbutton.grid(row=3, column=2, padx=scale(5), pady=scale(0))
 
 # Output Path
-outdirinput = ttk.Entry(frame1, width=19)
-outdirinput.place(x=10,y=300,height=30)
-pathlabel2 = ttk.Label(frame1,text="Output Path:",font=("Helvetica",7,"bold"))
-pathlabel2.place(x=10,y=280)
-pathbutton2 = ttk.Button(frame1,text="Select",command=selectdir)
-pathbutton2.place(x=230,y=300)
+pathlabel2 = ttk.Label(frame1, text="Output Path:", font=("Helvetica", scale(4), "bold"))
+pathlabel2.grid(row=4, column=0, sticky="w",columnspan=2,pady=scale((15,0)))
+
+outdirinput = ttk.Entry(frame1)
+outdirinput.grid(row=5, column=0, pady=scale(0), sticky="ew", columnspan=2)
+
+pathbutton2 = ttk.Button(frame1, text="Select", command=selectdir)
+pathbutton2.grid(row=5, column=2, padx=scale(5), pady=scale(0))
 
 # License
-licensetext = ttk.Label(frame1, text="nmcfs © MIT License - 2025 Rosafy",foreground="blue", cursor="hand2", font=("Helvetica",6),wraplength=100, justify="center")
-licensetext.place(x=100, y=470)
+licensetext = ttk.Label(
+    frame1,
+    text="nmcfs \u00a9 MIT License - 2025 Rosafy",
+    foreground="blue",
+    cursor="hand2",
+    font=("Helvetica", scale(4)),
+    wraplength=scale(70),
+    justify="center",
+)
+licensetext.grid(row=9, column=0, columnspan=3,pady=0)
 licensetext.bind("<Button-1>", lambda e: urlopen("https://github.com/rosafy/nmcfs/blob/main/LICENSE"))
 ToolTip(licensetext, msg="Read License")
+
+# Bottom Buttons
+buttons = ttk.Frame(frame1)
+buttons.grid(row=8, column=0, columnspan=3,pady=scale((15,0)))
+quitbutton = ttk.Button(buttons, text="Quit", command=root.destroy,width=scale(7))
+quitbutton.grid(row=0, column=1, sticky="sw",padx=scale((2.5,0)))
+
+compilebutton = ttk.Button(buttons, text="Compile", command=compile, width=scale(7))
+compilebutton.grid(row=0, column=0, sticky="se",padx=scale((0,2.5)))
 
 # Run
 root.mainloop()
