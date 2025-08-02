90ab34fc8b8cda0652af93a8979a15795adc2358
rosafy <emmadomeisen@gmail.com>
2025-08-02 20:05:22 +0200

    fuck this shit

--- cmdui.py
+++
@@ -1,8 +0,0 @@
-#from parser import *
-from termcolor import colored
-import time
-print(colored('nmcfs Compiler', 'white', attrs=['bold']))
-time.sleep(0.5)
-print("© 2025 Rosafy - MIT License")
-time.sleep(0.5)
-filepath = input("Paste path of file to compile: ")
